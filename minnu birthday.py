e195e49e51a2e8ac61eb001ef0d6f2e8d29df89b
TANA <tanachary33@gmail.com>
2023-04-27 09:23:28 +0530

    python files

---
+++ minnu birthday.py
@@ -0,0 +1,31 @@
+import time
+from random import randint
+for i in range(1, 85):
+    print(" ")
+space= ' '
+for i in range(1, 1000):
+    count = randint(1, 100)
+    while count > 0:
+        space += ' '
+        count -= 1
+
+    if i%10==0:
+        print(space + "HAPPY BIRTHDAY SRILAKSHMI_VADHINA")
+    elif i%9==0:
+        print(space + '🎂🎂🎂')
+    elif i%5 == 0:
+        print(space + '🎇🎇✨🎆✨')
+    elif i%8 == 0:
+        print(space + '🎉🎉🎊🎊🎉')
+    elif i%7 == 0:
+        print(space + '🎁🎁🎁')
+    elif i%6 == 0:
+        print(space + '💐💐💐')
+    else:
+        print(space + '🎈🎈🎈🎈')
+    space = ' '
+    time.sleep(0.2)
+
+
+
+
